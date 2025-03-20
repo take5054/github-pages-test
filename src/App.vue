95cf671ce4025dfec13725a5b5b70f3f9a71ecb6
take5054 <78195524+take5054@users.noreply.github.com>
2025-03-21 05:35:33 +0900

    add Vuetify 3

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-app>
+    <v-app-bar app>
+      <v-toolbar-title>github-pages-test</v-toolbar-title>
+    </v-app-bar>
+    <v-main>
+      <router-view />
+    </v-main>
+    <v-footer app>
+      <v-container>
+        <v-row>
+          <v-col class="text-center">
+            <span>take</span>
+          </v-col>
+          <v-divider />
+          <v-col class="text-center ">
+            <span>&copy; 2025</span>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-footer>
+  </v-app>
+</template>
+
+<script lang="ts" setup>
+  //
+</script>
---
+++ src/pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col>
+        <v-card href="/sub">
+          <v-card-title>Sub Page</v-card-title>
+          <v-divider />
+          <v-card-text>
+            <p>さぶぺーじへ</p>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script lang="ts" setup>
+//
+</script>
